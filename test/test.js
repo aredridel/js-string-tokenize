16b489b53592c33286d93673ce1d06052b4784b7
Aria Stewart <aredridel@nbtsc.org>
2017-12-03 01:31:22 -0500

    De-module and de-flowify and de-chaiifh

--- test/test.js
+++ test/test.js
@@ -1,13 +1,6 @@
-/* @flow */
-require('source-map-support').install();
-import 'babel-polyfill';
-import {assert} from 'chai';
-
-import {sortByLength}             from '../lib/lib.js';
-import {tokenize1}                from '../lib/lib.js';
-import {tokenize, nonDelimTokens} from '../lib/index.js';
-
-import type {TokenT} from '../lib/lib.js';
+const assert = require('assert');
+const {sortByLength, tokenize1} = require('../lib/lib.js');
+const {tokenize, nonDelimTokens} = require('../lib/index.js');
 
 console.log(new Date());
 
@@ -15,7 +8,7 @@ describe('sortByLength', function() {
     it('case 1', function() {
         const a = ['y', 'ab', 'zzz'];
         const aOrigJson = JSON.stringify(a);
-        const b: Array<string> = sortByLength(a);
+        const b = sortByLength(a);
         assert.strictEqual(JSON.stringify(a), aOrigJson);
         const bExpectedJSON = '["y","ab","zzz"]';
         assert.strictEqual(JSON.stringify(b), bExpectedJSON);
@@ -26,7 +19,7 @@ describe('sortByLength', function() {
 describe('tokenize', function() {
     describe('single delimiter, single occurence', function() {
         it('case 1', function() {
-            const tokens: Array<TokenT> = tokenize('aba', ['b']);
+            const tokens = tokenize('aba', ['b']);
             assert.strictEqual(JSON.stringify(tokens), '[{"v":"a","isDelim":false},{"v":"b","isDelim":true},{"v":"a","isDelim":false}]');
             {
                 const nonDelimTokensV = nonDelimTokens('aba', ['b']);
@@ -35,34 +28,34 @@ describe('tokenize', function() {
         });
 
         it('case 2', function() {
-            const tokens: Array<TokenT> = tokenize('ab', ['b']);
+            const tokens = tokenize('ab', ['b']);
             assert.strictEqual(JSON.stringify(tokens), '[{"v":"a","isDelim":false},{"v":"b","isDelim":true}]');
         });
 
         it('case 3', function() {
-            const tokens: Array<TokenT> = tokenize('ba', ['b']);
+            const tokens = tokenize('ba', ['b']);
             assert.strictEqual(JSON.stringify(tokens), '[{"v":"b","isDelim":true},{"v":"a","isDelim":false}]');                    
         });
         it('case 4', function() {
-            const tokens: Array<TokenT> = tokenize('b', ['b']);
+            const tokens = tokenize('b', ['b']);
             assert.strictEqual(JSON.stringify(tokens), '[{"v":"b","isDelim":true}]');
         });
 
         it('case 5', function() {
-            const tokens: Array<TokenT> = tokenize('a', ['b']);
+            const tokens = tokenize('a', ['b']);
             assert.strictEqual(JSON.stringify(tokens), '[{"v":"a","isDelim":false}]');
         });
         it('case 6', function() {
-            const tokens: Array<TokenT> = tokenize('a', ['ba']);
+            const tokens = tokenize('a', ['ba']);
             assert.strictEqual(JSON.stringify(tokens), '[{"v":"a","isDelim":false}]');
         });
 
         it('case 7', function() {
-            const tokens: Array<TokenT> = tokenize('  aba  ', ['b']);
+            const tokens = tokenize('  aba  ', ['b']);
             assert.strictEqual(JSON.stringify(tokens), '[{"v":"  a","isDelim":false},{"v":"b","isDelim":true},{"v":"a  ","isDelim":false}]');
         });
         it('case 8', function() {
-            const tokens: Array<TokenT> = tokenize('  aba  ', ['ba']);
+            const tokens = tokenize('  aba  ', ['ba']);
             assert.strictEqual(JSON.stringify(tokens), '[{"v":"  a","isDelim":false},{"v":"ba","isDelim":true},{"v":"  ","isDelim":false}]');
         });                                                
     }); 
@@ -70,11 +63,11 @@ describe('tokenize', function() {
 
     describe('single delimiter, double occurence', function() {
         it('case 1', function() {
-            const tokens: Array<TokenT> = tokenize('aba', ['a']);
+            const tokens = tokenize('aba', ['a']);
             assert.strictEqual(JSON.stringify(tokens), '[{"v":"a","isDelim":true},{"v":"b","isDelim":false},{"v":"a","isDelim":true}]');
         });
         it('case 2', function() {
-            const tokens: Array<TokenT> = tokenize('  aba  ', ['a']);
+            const tokens = tokenize('  aba  ', ['a']);
             assert.strictEqual(JSON.stringify(tokens), '[{"v":"  ","isDelim":false},{"v":"a","isDelim":true},{"v":"b","isDelim":false},{"v":"a","isDelim":true},{"v":"  ","isDelim":false}]');
         });
     });
@@ -82,63 +75,63 @@ describe('tokenize', function() {
         it('case 1', function() {
             const searchPatterns = [ ['a', 'b'], ['b', 'a'], ['x', 'xx', 'a', 'xxx', 'b'] ];
             for (let i = 0 ; i < searchPatterns.length ; i++) {
-                const tokens: Array<TokenT> = tokenize('aba', searchPatterns[i]);
+                const tokens = tokenize('aba', searchPatterns[i]);
                 assert.strictEqual(JSON.stringify(tokens), '[{"v":"a","isDelim":true},{"v":"b","isDelim":true},{"v":"a","isDelim":true}]');
             }
         });
         it('case 1b', function() {
             const searchPatterns = [ ['a', 'b'], ['b', 'a'], ['x', 'xx', 'a', 'xxx', 'b'] ];
             for (let i = 0 ; i < searchPatterns.length ; i++) {
-                const tokens: Array<TokenT> = tokenize('aba', searchPatterns[i], true, true);
+                const tokens = tokenize('aba', searchPatterns[i], true, true);
                 assert.strictEqual(JSON.stringify(tokens), '[{"v":"aba","isDelim":true}]');
             }
         });                
         it('case 2', function() {
             const searchPatterns = [ ['a', 'b'], ['b', 'a'], ['x', 'xx', 'a', 'xxx', 'b'] ];
             for (let i = 0 ; i < searchPatterns.length ; i++) {
-                const tokens: Array<TokenT> = tokenize('abccda', searchPatterns[i]);
+                const tokens = tokenize('abccda', searchPatterns[i]);
                 assert.strictEqual(JSON.stringify(tokens), '[{"v":"a","isDelim":true},{"v":"b","isDelim":true},{"v":"ccd","isDelim":false},{"v":"a","isDelim":true}]');
             }
         });
         it('case 2b', function() {
             const searchPatterns = [ ['a', 'b'], ['b', 'a'], ['x', 'xx', 'a', 'xxx', 'b'] ];
             for (let i = 0 ; i < searchPatterns.length ; i++) {
-                const tokens: Array<TokenT> = tokenize('abccda', searchPatterns[i], true, true);
+                const tokens = tokenize('abccda', searchPatterns[i], true, true);
                 assert.strictEqual(JSON.stringify(tokens), '[{"v":"ab","isDelim":true},{"v":"ccd","isDelim":false},{"v":"a","isDelim":true}]');
             }
         });                
         it('case 3', function() {
             const searchPatterns = [ ['a', 'b'], ['b', 'a'], ['x', 'xx', 'a', 'xxx', 'b'] ];
             for (let i = 0 ; i < searchPatterns.length ; i++) {
-                const tokens: Array<TokenT> = tokenize(' f f abccda g', searchPatterns[i]);
+                const tokens = tokenize(' f f abccda g', searchPatterns[i]);
                 assert.strictEqual(JSON.stringify(tokens), '[{"v":" f f ","isDelim":false},{"v":"a","isDelim":true},{"v":"b","isDelim":true},{"v":"ccd","isDelim":false},{"v":"a","isDelim":true},{"v":" g","isDelim":false}]');
             }
         });
         it('case 3b', function() {
             const searchPatterns = [ ['a', 'b'], ['b', 'a'], ['x', 'xx', 'a', 'xxx', 'b'] ];
             for (let i = 0 ; i < searchPatterns.length ; i++) {
-                const tokens: Array<TokenT> = tokenize(' f f abccda g', searchPatterns[i], true, true);
+                const tokens = tokenize(' f f abccda g', searchPatterns[i], true, true);
                 assert.strictEqual(JSON.stringify(tokens), '[{"v":" f f ","isDelim":false},{"v":"ab","isDelim":true},{"v":"ccd","isDelim":false},{"v":"a","isDelim":true},{"v":" g","isDelim":false}]');
             }
         });
         it('case 4', function() {
             const searchPatterns = [ ['a', 'ba'], ['ba', 'a'], ['y', 'yy', 'ba', 'yyy', 'a'] ];
             for (let i = 0 ; i < searchPatterns.length ; i++) {
-                const tokens: Array<TokenT> = tokenize('xxbaxx', searchPatterns[i]);
+                const tokens = tokenize('xxbaxx', searchPatterns[i]);
                 assert.strictEqual(JSON.stringify(tokens), '[{"v":"xx","isDelim":false},{"v":"ba","isDelim":true},{"v":"xx","isDelim":false}]');
             }
         });
         it('case 4b', function() {
             const searchPatterns = [ ['a', 'ba'], ['ba', 'a'], ['y', 'yy', 'ba', 'yyy', 'a'] ];
             for (let i = 0 ; i < searchPatterns.length ; i++) {
-                const tokens: Array<TokenT> = tokenize('xxbaxx', searchPatterns[i], true, true);
+                const tokens = tokenize('xxbaxx', searchPatterns[i], true, true);
                 assert.strictEqual(JSON.stringify(tokens), '[{"v":"xx","isDelim":false},{"v":"ba","isDelim":true},{"v":"xx","isDelim":false}]');
             }
         });                
         it('case 5', function() {
             const searchPatterns = [ ['a', 'ba'], ['ba', 'a'], ['y', 'yy', 'ba', 'yyy', 'a'] ];
             for (let i = 0 ; i < searchPatterns.length ; i++) {
-                const tokens: Array<TokenT> = tokenize('xxbaabaxx', searchPatterns[i]);
+                const tokens = tokenize('xxbaabaxx', searchPatterns[i]);
                 assert.strictEqual(JSON.stringify(tokens), '[{"v":"xx","isDelim":false},{"v":"ba","isDelim":true},{"v":"a","isDelim":true},{"v":"ba","isDelim":true},{"v":"xx","isDelim":false}]');
             }
         });
@@ -146,7 +139,7 @@ describe('tokenize', function() {
             const searchPatterns = [ ['a', 'ba'], ['ba', 'a'], ['y', 'yy', 'ba', 'yyy', 'a'] ];
             for (let i = 0 ; i < searchPatterns.length ; i++) {
                 const INPUT = 'xxbaabaxx';
-                const tokens: Array<TokenT> = tokenize(INPUT, searchPatterns[i], true, true);
+                const tokens = tokenize(INPUT, searchPatterns[i], true, true);
                 assert.strictEqual(JSON.stringify(tokens), '[{"v":"xx","isDelim":false},{"v":"baaba","isDelim":true},{"v":"xx","isDelim":false}]');
                 {
                     const nonDelimTokensV = nonDelimTokens(INPUT, ['b']);
